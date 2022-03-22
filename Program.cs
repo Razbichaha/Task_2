cf7592d83c564002a2535a20f524663353ab0c83
Notic <Notic@LAPTOP-2BCD8Q2I>
2022-03-22 23:28:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable_one = -32;
+            byte variable_two = 1;
+            bool variable_three = true;
+            short variable_four = 32600;
+            uint variable_five = 1000000;
+            byte variable_six = 255;
+            sbyte variable_seven = -128;
+            long variable_eight = 10000000000;
+            ulong variable_nine = 2000000000000;
+            double variable_ten = 10.2;
+        }
+    }
+}
